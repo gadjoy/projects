cf6d8b05a4dd6c03b9a5aaa4f6d5ae0fcb8095cf
Mdwaseem2002 <waseemmd907@gmail.com>
2024-03-29 17:15:46 +0530

    Initial docs

--- freelancer_auto/server/components/freelancer.py
+++ freelancer_auto/server/components/freelancer.py
@@ -1,27 +1,30 @@
 from freelancersdk.session import Session
 from freelancersdk.resources.projects import search_projects, get_project_by_id, place_project_bid
-from freelancersdk.resources.projects.exceptions import ProjectsNotFoundException
-from freelancersdk.resources.projects.helpers import create_search_projects_filter, create_get_projects_user_details_object, create_get_projects_project_details_object
-from freelancersdk.resources.users import get_self_user_id
+from freelancersdk.resources.projects.exceptions import \
+    ProjectsNotFoundException
+from freelancersdk.resources.projects.helpers import (
+    create_search_projects_filter,
+    create_get_projects_user_details_object,
+    create_get_projects_project_details_object,
+)
+from freelancersdk.resources.users \
+    import get_self_user_id
 from freelancersdk.exceptions import BidNotPlacedException
+
 from dotenv import load_dotenv
 import os
-import random
 
 load_dotenv()
 
-def _search_projects(query_str, page=1):
+def _search_projects(query_str):
     url = os.environ.get('FLN_URL')
     oauth_token = os.environ.get('FLN_OAUTH_TOKEN')
     session = Session(oauth_token=oauth_token, url=url)
 
-    # Introduce a random offset to vary the search results
-    random_offset = random.randint(0, 100)
-
     query = query_str
     search_filter = create_search_projects_filter(
-        sort_field='time_updated',
-        or_search_query=True,
+        sort_field= 'time_updated',
+        or_search_query= True,
         languages='en',
     )
 
@@ -29,11 +32,11 @@ def _search_projects(query_str, page=1):
         p = search_projects(
             session,
             query=query,
+            # ! Hardcoded limit=10 default
             active_only=True,
             search_filter=search_filter,
-            offset=(page - 1) * 10 + random_offset,  # Include random offset
-            limit=10,  # Limit to 10 projects per page
         )
+
     except ProjectsNotFoundException as e:
         print('Error message: {}'.format(e.message))
         print('Server response: {}'.format(e.error_code))
@@ -74,12 +77,15 @@ def _place_project_bid(project_id, amount, proposal):
         'project_id': int(project_id),
         'bidder_id': int(my_user_id),
         'amount': amount,
+        # !Hardcoded Period
         'period': 7,
+        # !Hardcoded Milestone
         'milestone_percentage': 100,
         'description': proposal,
     }
     try:
         return place_project_bid(session, **bid_data)
     except BidNotPlacedException as e:
+        # print(('Error message: %s' % e.message))
         print(('Error code: %s' % e.error_code))
         return None
