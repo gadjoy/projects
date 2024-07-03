360106b50d80a49206c93953f1b4d5e4acc73c58
Bhanodhay Rao <22bcs028@iiitdwd.ac.in>
2024-07-03 19:14:13 +0530

    chat-with-pdf & ai calling agent

---
+++ chat-with-pdf/copy_client/src/components/PdfViewWithChatbox.vue
@@ -0,0 +1,80 @@
+<template>
+    <div class="pdf-view-container">
+      <div class="pdf-list">
+        <button v-for="(pdf, index) in pdfs" :key="index" @click="selectPdf(pdf)">
+          {{ pdf.name }}
+          <span v-if="pdf.locked">🔒</span>
+        </button>
+      </div>
+      <div class="pdf-view">
+        <iframe v-if="selectedPdf" :src="selectedPdf.url" width="100%" height="100%"></iframe>
+        <p v-else>Select a PDF to view</p>
+      </div>
+      <div class="chatbox">
+        <h3>Chatbox</h3>
+        <!-- Chatbox implementation will go here -->
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        pdfs: JSON.parse(localStorage.getItem('pdfs')) || [],
+        selectedPdf: null,
+      };
+    },
+    methods: {
+      selectPdf(pdf) {
+        this.selectedPdf = pdf;
+      }
+    }
+  };
+  </script>
+  
+  <style scoped>
+  .pdf-view-container {
+    display: flex;
+    height: 100vh;
+    background-color: #3b5998;
+    color: #ffffff;
+  }
+  
+  .pdf-list {
+    width: 20%;
+    background-color: #8b9dc3;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+  
+  .pdf-list button {
+    margin: 10px 0;
+    padding: 10px;
+    border: none;
+    border-radius: 25px;
+    background-color: #000000;
+    color: #ffffff;
+    cursor: pointer;
+  }
+  
+  .pdf-view {
+    width: 40%;
+    background-color: #3b5998;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  
+  .chatbox {
+    width: 40%;
+    background-color: #000000;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+  </style>
+  
--- chat-with-pdf/copy_client/src/main.js
+++ chat-with-pdf/copy_client/src/main.js
@@ -1,6 +1,5 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
-import '@fortawesome/fontawesome-free/css/all.css';  // Add this line for Font Awesome
 
 createApp(App).use(router).mount('#app');
